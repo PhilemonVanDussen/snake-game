baa76e14a553140ec2a997afb77aba680c0d9436
PhilemonVanDussen <vandussenph709@northwested.org>
2025-04-23 12:57:18 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,25 @@
+# Pygame Configuration Module
+
+# Color constants (RBG)
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PINK = (255, 192, 203)
+ORANGE = (255, 165, 0)
+PURPLE = (157, 0, 255)
+CYAN = (0, 255, 255)
+YELLOW = (255, 255, 0)
+BROWN = (150, 75, 0)
+
+# Game window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Window title (caption)
+# Update the window title as needed
+TITLE = 'My Pygame Project'
+
+# Frame rate (frames per second)
+FPS = 60
